75f7af3c525a27b3a86cd77c0a5fdd4e5f6f4eb8
tarikkeskin <60899740+tarikkeskin@users.noreply.github.com>
2020-02-11 00:18:52 +0300

    Add files via upload

---
+++ HashTable.h
@@ -0,0 +1,187 @@
+#ifndef __HASHTABLE__
+#define __HASHTABLE__
+
+#include "HashUtils.h"
+
+// Do not modify the public interface of this class.
+// Otherwise, your code will note compile!
+template <class T>
+class HashTable {
+    struct Entry {
+        std::string Key;             // the key of the entry
+        T Value;   // the value of the entry
+        bool Deleted;        // flag indicating whether this entry is deleted
+        bool Active;         // flag indicating whether this item is currently used
+
+        Entry() : Key(), Value(), Deleted(false), Active(false) {}
+    };
+
+    struct Bucket {
+        Entry entries[3];
+    };
+
+    int _capacity; // INDICATES THE SIZE OF THE TABLE
+    int _size; // INDICATES THE NUMBER OF ITEMS IN THE TABLE
+
+    Bucket* _table; // HASH TABLE
+
+    // You can define private methods and variables
+
+public:
+    // TODO: IMPLEMENT THESE FUNCTIONS.
+    // CONSTRUCTORS, ASSIGNMENT OPERATOR, AND THE DESTRUCTOR
+    HashTable();
+    HashTable(const HashTable<T>& rhs);
+    HashTable<T>& operator=(const HashTable<T>& rhs);
+    ~HashTable();
+
+    // TODO: IMPLEMENT THIS FUNCTION.
+    // INSERT THE ENTRY IN THE HASH TABLE WITH THE GIVEN KEY & VALUE
+    // IF THE GIVEN KEY ALREADY EXISTS, THE NEW VALUE OVERWRITES
+    // THE ALREADY EXISTING ONE.
+    // IF LOAD FACTOR OF THE TABLE IS BIGGER THAN 0.5,
+    // RESIZE THE TABLE WITH THE NEXT PRIME NUMBER.
+    void Insert(std::string key, const T& value);
+
+    // TODO: IMPLEMENT THIS FUNCTION.
+    // DELETE THE ENTRY WITH THE GIVEN KEY FROM THE TABLE
+    // IF THE GIVEN KEY DOES NOT EXIST IN THE TABLE, JUST RETURN FROM THE FUNCTION
+    // HINT: YOU SHOULD UPDATE ACTIVE & DELETED FIELDS OF THE DELETED ENTRY.
+    void Delete(std::string key);
+
+    // TODO: IMPLEMENT THIS FUNCTION.
+    // IT SHOULD RETURN THE VALUE THAT CORRESPONDS TO THE GIVEN KEY.
+    // IF THE KEY DOES NOT EXIST, THIS FUNCTION MUST RETURN T()
+    T Get(std::string key) const;
+
+    // TODO: IMPLEMENT THIS FUNCTION.
+    // AFTER THIS FUNCTION IS EXECUTED THE TABLE CAPACITY MUST BE
+    // EQUAL TO newCapacity AND ALL THE EXISTING ITEMS MUST BE REHASHED
+    // ACCORDING TO THIS NEW CAPACITY.
+    // WHEN CHANGING THE SIZE, YOU MUST REHASH ALL OF THE ENTRIES FROM 0TH ENTRY TO LAST ENTRY
+    void Resize(int newCapacity);
+
+    // TODO: IMPLEMENT THIS FUNCTION.
+    // RETURNS THE AVERAGE NUMBER OF PROBES FOR SUCCESSFUL SEARCH
+    double getAvgSuccessfulProbe();
+
+    // TODO: IMPLEMENT THIS FUNCTION.
+    // RETURNS THE AVERAGE NUMBER OF PROBES FOR UNSUCCESSFUL SEARCH
+    double getAvgUnsuccessfulProbe();
+
+    // THE IMPLEMENTATION OF THESE FUNCTIONS ARE GIVEN TO YOU
+    // DO NOT MODIFY!
+    int Capacity() const;
+    int Size() const;
+};
+
+
+
+template <class T>
+HashTable<T>::HashTable() {
+  _table==NULL;
+  _capacity=0;
+  _size=0;
+// TODO: CONSTRUCTOR
+
+}
+
+template <class T>
+HashTable<T>::HashTable(const HashTable<T>& rhs)
+
+// TODO: COPY CONSTRUCTOR
+
+for (int i = 0; i <_capacity; i++)
+  {
+      _table=new Bucket[_capacity];
+
+      for (int j = 0; j < 3; j++)
+      {
+          _table->entries[j]=Entry();
+      }
+  }
+   for (int i = 0; i <_capacity; i++)
+  {
+      for (int j = 0; j < 3; j++)
+      {
+          _table->entries[j].Key=rhs._table->entries[j].Key;
+          _table->entries[j].Value=rhs._table->entries[j].Value;
+      }
+
+  }
+}
+
+template <class T>
+HashTable<T>& HashTable<T>::operator=(const HashTable<T>& rhs) {
+// TODO: OPERATOR=
+
+}
+
+template <class T>
+HashTable<T>::~HashTable() {
+// TODO: DESTRUCTOR
+
+}
+
+template <class T>
+void HashTable<T>::Insert(std::string key, const T& value) {
+// TODO: IMPLEMENT THIS FUNCTION.
+// INSERT THE ENTRY IN THE HASH TABLE WITH THE GIVEN KEY & VALUE
+// IF THE GIVEN KEY ALREADY EXISTS, THE NEW VALUE OVERWRITES
+// THE ALREADY EXISTING ONE. IF LOAD FACTOR OF THE TABLE IS BIGGER THAN 0.5,
+// RESIZE THE TABLE WITH THE NEXT PRIME NUMBER.
+
+}
+
+template <class T>
+void HashTable<T>::Delete(std::string key) {
+// TODO: IMPLEMENT THIS FUNCTION.
+// DELETE THE ENTRY WITH THE GIVEN KEY FROM THE TABLE
+// IF THE GIVEN KEY DOES NOT EXIST IN THE TABLE, JUST RETURN FROM THE FUNCTION
+// HINT: YOU SHOULD UPDATE ACTIVE & DELETED FIELDS OF THE DELETED ENTRY.
+
+}
+
+template <class T>
+T HashTable<T>::Get(std::string key) const {
+// TODO: IMPLEMENT THIS FUNCTION. IT SHOULD RETURN THE VALUE THAT
+// IT SHOULD RETURN THE VALUE THAT CORRESPONDS TO THE GIVEN KEY.
+// IF THE KEY DOES NOT EXIST, THIS FUNCTION MUST RETURN T()
+
+}
+
+template <class T>
+void HashTable<T>::Resize(int newCapacity) {
+// TODO: IMPLEMENT THIS FUNCTION. AFTER THIS FUNCTION IS EXECUTED
+// THE TABLE CAPACITY MUST BE EQUAL TO newCapacity AND ALL THE
+// EXISTING ITEMS MUST BE REHASHED ACCORDING TO THIS NEW CAPACITY.
+// WHEN CHANGING THE SIZE, YOU MUST REHASH ALL OF THE ENTRIES FROM 0TH ENTRY TO LAST ENTRY
+
+}
+
+
+template <class T>
+double HashTable<T>::getAvgSuccessfulProbe() {
+// TODO: IMPLEMENT THIS FUNCTION.
+// RETURNS THE AVERAGE NUMBER OF PROBES FOR SUCCESSFUL SEARCH
+
+}
+
+template <class T>
+double HashTable<T>::getAvgUnsuccessfulProbe() {
+// TODO: IMPLEMENT THIS FUNCTION.
+// RETURNS THE AVERAGE NUMBER OF PROBES FOR UNSUCCESSFUL SEARCH
+
+}
+
+template <class T>
+int HashTable<T>::Capacity() const {
+    return _capacity;
+}
+
+template <class T>
+int HashTable<T>::Size() const {
+    return _size;
+}
+
+#endif
