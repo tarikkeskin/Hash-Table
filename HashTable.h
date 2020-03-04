eca1328e898b4b1a7a48b4391adc700caa9275f0
tarikkeskin <60899740+tarikkeskin@users.noreply.github.com>
2020-03-05 00:55:13 +0300

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -82,6 +82,7 @@ HashTable<T>::HashTable() {
   _table==NULL;
   _capacity=0;
   _size=0;
+    s=0;
 // TODO: CONSTRUCTOR
 
 }
@@ -89,30 +90,34 @@ HashTable<T>::HashTable() {
 template <class T>
 HashTable<T>::HashTable(const HashTable<T>& rhs)
 
-// TODO: COPY CONSTRUCTOR
-
-for (int i = 0; i <_capacity; i++)
-  {
-      _table=new Bucket[_capacity];
-
-      for (int j = 0; j < 3; j++)
-      {
-          _table->entries[j]=Entry();
-      }
-  }
-   for (int i = 0; i <_capacity; i++)
-  {
-      for (int j = 0; j < 3; j++)
-      {
-          _table->entries[j].Key=rhs._table->entries[j].Key;
-          _table->entries[j].Value=rhs._table->entries[j].Value;
-      }
-
-  }
+    _table=NULL;
+    _table = new Bucket[rhs.Capacity()];
+    for(int i=0;i<rhs.Capacity();i++){
+        for(int j=0;j<3;j++){
+            _table[i].entries[j] = rhs._table[i].entries[j];
+        }
+    }
+    _size=rhs.Size();
+    _capacity=rhs.Capacity();
+    s=rhs.s;
 }
-
 template <class T>
 HashTable<T>& HashTable<T>::operator=(const HashTable<T>& rhs) {
+    if(_table){
+        delete[] _table;
+    }
+    _table=NULL;
+    _table = new Bucket[rhs.Capacity()];
+    for(int i=0;i<rhs.Capacity();i++){
+        for(int j=0;j<3;j++){
+            _table[i].entries[j] = rhs._table[i].entries[j];
+        }
+    }
+    _size=rhs.Size();
+    _capacity=rhs.Capacity();
+    s=rhs.s;
+
+    return *this;
 // TODO: OPERATOR=
 
 }
@@ -120,11 +125,55 @@ HashTable<T>& HashTable<T>::operator=(const HashTable<T>& rhs) {
 template <class T>
 HashTable<T>::~HashTable() {
 // TODO: DESTRUCTOR
-
+    delete [] _table;
+    _capacity=0;
+    _size=0;
+    s=0;
 }
 
 template <class T>
 void HashTable<T>::Insert(std::string key, const T& value) {
+    if(_table==NULL){
+        _capacity=NextCapacity(_capacity);
+        _table = new Bucket[_capacity];
+    }
+    if((float)_size/(3*_capacity) > 0.5){
+        Resize(NextCapacity(_capacity));
+        //std::cout<<"resizing..   "<< _capacity<<std::endl;
+    }
+
+    int adres = Hash(key) % _capacity;
+    int artis = 1;
+    bool done = false;
+
+    while(!done){
+        for(int i=0;i<3;i++){
+            if(key == _table[adres].entries[i].Key){
+                done=true;
+                break;
+            }
+            if(_table[adres].entries[i].Active == false){
+                _table[adres].entries[i].Value = value;
+                _table[adres].entries[i].Active = true;
+                _table[adres].entries[i].Key = key;
+                _size++;
+                done = true;
+                break;
+            }
+        }
+        adres = (Hash(key) % _capacity + artis*artis) % _capacity;
+        artis++;
+        s++;
+    }
+
+
+    //_table[Hash(key)%_capacity].entries[0].Value = value;
+    //std::cout<<"inserting"<<std::endl;
+
+
+    /*_table[300].entries[1].Key="asdsg";
+    std::cout<< _table[300].entries[1].Key <<std::endl;*/
+
 // TODO: IMPLEMENT THIS FUNCTION.
 // INSERT THE ENTRY IN THE HASH TABLE WITH THE GIVEN KEY & VALUE
 // IF THE GIVEN KEY ALREADY EXISTS, THE NEW VALUE OVERWRITES
@@ -135,6 +184,19 @@ void HashTable<T>::Insert(std::string key, const T& value) {
 
 template <class T>
 void HashTable<T>::Delete(std::string key) {
+    for(int i=0;i<_capacity;i++){
+        for(int j=0;j<3;j++){
+            if(_table[i].entries[j].Key == key){
+                _table[i].entries[j] = Entry();
+                _table[i].entries[j].Active=false;
+                _table[i].entries[j].Deleted=true;
+                _size--;
+                break;
+            }
+        }
+    }
+    return;
+
 // TODO: IMPLEMENT THIS FUNCTION.
 // DELETE THE ENTRY WITH THE GIVEN KEY FROM THE TABLE
 // IF THE GIVEN KEY DOES NOT EXIST IN THE TABLE, JUST RETURN FROM THE FUNCTION
@@ -144,6 +206,14 @@ void HashTable<T>::Delete(std::string key) {
 
 template <class T>
 T HashTable<T>::Get(std::string key) const {
+    for(int i=0;i<_capacity;i++){
+        for(int j=0;j<3;j++){
+            if(_table[i].entries[j].Key == key){
+                return _table[i].entries[j].Value;
+            }
+        }
+    }
+    return T();
 // TODO: IMPLEMENT THIS FUNCTION. IT SHOULD RETURN THE VALUE THAT
 // IT SHOULD RETURN THE VALUE THAT CORRESPONDS TO THE GIVEN KEY.
 // IF THE KEY DOES NOT EXIST, THIS FUNCTION MUST RETURN T()
@@ -152,6 +222,34 @@ T HashTable<T>::Get(std::string key) const {
 
 template <class T>
 void HashTable<T>::Resize(int newCapacity) {
+    //Bucket *temp = new Bucket[NextCapacity(_capacity)];
+    s=1;
+
+    T *temp = new T[_size];
+    int index=0;
+    //std::cout<<"resizuuuying.."<<std::endl;
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<_capacity;j++){
+            if(_table[j].entries[i].Active == true){
+                temp[index] = _table[j].entries[i].Value;
+                index++;
+            }
+        }
+    }
+
+    delete[] _table;
+    _table = new Bucket[newCapacity];
+    _size = 0;
+    _capacity = newCapacity;
+
+    for(int i=0;i<index;i++){
+        Insert(temp[i].getIsbn(),temp[i]);
+    }
+
+    delete[] temp;
+    temp=NULL;
+
 // TODO: IMPLEMENT THIS FUNCTION. AFTER THIS FUNCTION IS EXECUTED
 // THE TABLE CAPACITY MUST BE EQUAL TO newCapacity AND ALL THE
 // EXISTING ITEMS MUST BE REHASHED ACCORDING TO THIS NEW CAPACITY.
@@ -162,6 +260,7 @@ void HashTable<T>::Resize(int newCapacity) {
 
 template <class T>
 double HashTable<T>::getAvgSuccessfulProbe() {
+    return (double)s/_size;
 // TODO: IMPLEMENT THIS FUNCTION.
 // RETURNS THE AVERAGE NUMBER OF PROBES FOR SUCCESSFUL SEARCH
 
@@ -169,6 +268,33 @@ double HashTable<T>::getAvgSuccessfulProbe() {
 
 template <class T>
 double HashTable<T>::getAvgUnsuccessfulProbe() {
+    int us = 0;
+    int adres = 0;
+    int artis = 1;
+    bool done = false;
+
+    for(int i=0;i<_capacity;i++){
+        adres = i;
+        artis = 1;
+        done = false;
+        while(!done){
+            if(_table[adres].entries[0].Active == true){
+                if(_table[adres].entries[1].Active == true){
+                    if(_table[adres].entries[2].Active == true){
+                        adres = (i + artis*artis) % _capacity;
+                        artis++;
+                        us++;
+                        continue;
+                    }
+                }
+            }
+            done = true;
+            us++;
+        }
+    }
+
+    return (double)us/_capacity;
+
 // TODO: IMPLEMENT THIS FUNCTION.
 // RETURNS THE AVERAGE NUMBER OF PROBES FOR UNSUCCESSFUL SEARCH
 
